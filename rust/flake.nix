80edf3fcea42f6fd2296ff4a4f755168c32583be
zhk-kk <zhkcake@gmail.com>
2025-09-03 19:20:15 +0300

    rust

--- rust/flake.nix
+++ rust/flake.nix
@@ -2,25 +2,36 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
     {
       nixpkgs,
       flake-utils,
+      rust-overlay,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
+        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
-          inherit system;
+          inherit system overlays;
         };
       in
       {
         formatter = pkgs.nixfmt-rfc-style;
 
         devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            openssl
+            pkg-config
+            rust-bin.stable.latest.default
+            rust-analyzer
+          ];
+
           shellHook = ''
             export PS1="\n\033[1;34m(devshell) \w$ \033[0m";
           '';
